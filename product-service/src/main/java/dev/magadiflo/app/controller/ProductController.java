074186f93c513f845080b117f74f42be0ba4ce2c
Martín <magadiflo@gmail.com>
2024-12-03 10:22:30 -0500

    Múltiples instancias del product-service

--- product-service/src/main/java/dev/magadiflo/app/controller/ProductController.java
+++ product-service/src/main/java/dev/magadiflo/app/controller/ProductController.java
@@ -1,13 +1,19 @@
 package dev.magadiflo.app.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/api/v1/products")
 public class ProductController {
+
+    private final Environment environment;
+
     @GetMapping
     public String getProduct() {
-        return "HTTP GET Handled";
+        return "[%s]HTTP GET Handled".formatted(this.environment.getProperty("local.server.port"));
     }
 
     @PostMapping
