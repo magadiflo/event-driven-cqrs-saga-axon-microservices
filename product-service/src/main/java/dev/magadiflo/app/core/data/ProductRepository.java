e74ba60c42ad454dcbe3401d26b8e67f2dc89611
Martín <magadiflo@gmail.com>
2025-01-23 10:23:21 -0500

    Crea ProductRepository

---
+++ product-service/src/main/java/dev/magadiflo/app/core/data/ProductRepository.java
@@ -0,0 +1,11 @@
+package dev.magadiflo.app.core.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, String> {
+    Optional<ProductEntity> findByProductId(String productId);
+
+    Optional<ProductEntity> findByProductIdOrTitle(String productId, String title);
+}
