081847db52516b2a2b8903ac2c038ad7f468ea7c
Martín <magadiflo@gmail.com>
2025-01-23 12:04:18 -0500

    Implementando el método anotado con @EventHandler

--- product-service/src/main/java/dev/magadiflo/app/query/ProductEventsHandler.java
+++ product-service/src/main/java/dev/magadiflo/app/query/ProductEventsHandler.java
@@ -1,14 +1,28 @@
 package dev.magadiflo.app.query;
 
+import dev.magadiflo.app.core.data.ProductEntity;
+import dev.magadiflo.app.core.data.ProductRepository;
 import dev.magadiflo.app.core.event.ProductCreatedEvent;
+import lombok.RequiredArgsConstructor;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class ProductEventsHandler {
+
+    private final ProductRepository productRepository;
 
     @EventHandler
     public void on(ProductCreatedEvent event) {
 
+        ProductEntity productEntity = ProductEntity.builder()
+                .productId(event.getProductId())
+                .title(event.getTitle())
+                .price(event.getPrice())
+                .quantity(event.getQuantity())
+                .build();
+
+        this.productRepository.save(productEntity);
     }
 }
