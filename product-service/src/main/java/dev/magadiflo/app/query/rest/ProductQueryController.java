c1df07764b2fc1f422ee025a0b9e02cad2d27278
Martín <magadiflo@gmail.com>
2025-01-27 23:01:13 -0500

    Crea RestController

---
+++ product-service/src/main/java/dev/magadiflo/app/query/rest/ProductQueryController.java
@@ -0,0 +1,9 @@
+package dev.magadiflo.app.query.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/products")
+public class ProductQueryController {
+}
