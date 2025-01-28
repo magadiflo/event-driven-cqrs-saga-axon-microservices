f5469b597da47627662ac10b622fd8d3d15a7636
Martín <magadiflo@gmail.com>
2025-01-28 10:13:55 -0500

    Crea endpoint del servicio web para obtener productos

--- product-service/src/main/java/dev/magadiflo/app/query/rest/ProductQueryController.java
+++ product-service/src/main/java/dev/magadiflo/app/query/rest/ProductQueryController.java
@@ -1,9 +1,19 @@
 package dev.magadiflo.app.query.rest;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/products")
 public class ProductQueryController {
+
+    @GetMapping
+    public ResponseEntity<List<ProductRestModel>> getProducts() {
+        return null;
+    }
+
 }
