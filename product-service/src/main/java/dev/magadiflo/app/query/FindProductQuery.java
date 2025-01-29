f4def4977c5eb477b02633b1986926e6ac393503
Martín <magadiflo@gmail.com>
2025-01-29 15:35:45 -0500

    Realiza consulta con el QueryGateway

---
+++ product-service/src/main/java/dev/magadiflo/app/query/FindProductQuery.java
@@ -0,0 +1,5 @@
+package dev.magadiflo.app.query;
+
+public class FindProductQuery {
+    // No hay atributos porque solo queremos obtener todos los productos
+}
--- product-service/src/main/java/dev/magadiflo/app/query/rest/ProductQueryController.java
+++ product-service/src/main/java/dev/magadiflo/app/query/rest/ProductQueryController.java
@@ -1,5 +1,9 @@
 package dev.magadiflo.app.query.rest;
 
+import dev.magadiflo.app.query.FindProductQuery;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/api/v1/products")
 public class ProductQueryController {
 
+    private final QueryGateway queryGateway;
+
     @GetMapping
     public ResponseEntity<List<ProductRestModel>> getProducts() {
-        return null;
+        FindProductQuery findProductQuery = new FindProductQuery();
+
+        List<ProductRestModel> products = this.queryGateway
+                .query(findProductQuery, ResponseTypes.multipleInstancesOf(ProductRestModel.class))
+                .join();
+
+        return ResponseEntity.ok(products);
     }
 
 }
