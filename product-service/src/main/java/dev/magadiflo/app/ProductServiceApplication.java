24b56e0117a27ad8eae2bb877beab599f4820b75
Martín <magadiflo@gmail.com>
2025-02-04 10:24:54 -0500

    Registra el Message Dispatch Interceptor

--- product-service/src/main/java/dev/magadiflo/app/ProductServiceApplication.java
+++ product-service/src/main/java/dev/magadiflo/app/ProductServiceApplication.java
@@ -1,13 +1,23 @@
 package dev.magadiflo.app;
 
+import dev.magadiflo.app.command.interceptors.CreateProductCommandInterceptor;
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+    @Autowired
+    public void registerCreateProductCommandInterceptor(ApplicationContext context, CommandBus commandBus) {
+        CreateProductCommandInterceptor bean = context.getBean(CreateProductCommandInterceptor.class);
+        commandBus.registerDispatchInterceptor(bean);
     }
 
 }
