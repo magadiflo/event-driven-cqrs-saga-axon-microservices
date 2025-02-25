7fcb0c9ca68a3844ca06ae9036578945697ed94e
Martín <magadiflo@gmail.com>
2025-02-25 17:16:07 -0500

    Registra el ListenerInvocationErrorHandler

--- product-service/src/main/java/dev/magadiflo/app/ProductServiceApplication.java
+++ product-service/src/main/java/dev/magadiflo/app/ProductServiceApplication.java
@@ -1,7 +1,9 @@
 package dev.magadiflo.app;
 
 import dev.magadiflo.app.command.interceptors.CreateProductCommandInterceptor;
+import dev.magadiflo.app.core.errorhandling.ProductServiceEventErrorHandler;
 import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.config.EventProcessingConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,6 +20,11 @@ public class ProductServiceApplication {
     public void registerCreateProductCommandInterceptor(ApplicationContext context, CommandBus commandBus) {
         CreateProductCommandInterceptor bean = context.getBean(CreateProductCommandInterceptor.class);
         commandBus.registerDispatchInterceptor(bean);
+    }
+
+    @Autowired
+    public void configure(EventProcessingConfigurer config) {
+        config.registerListenerInvocationErrorHandler("product-group", configuration -> new ProductServiceEventErrorHandler());
     }
 
 }
